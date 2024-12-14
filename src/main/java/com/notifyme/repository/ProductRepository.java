dbe4d349f87bf589059aae60aea3beb3ef1fd46c
rustywhite404 <rustywhite404@gmail.com>
2024-12-14 15:40:55 +0900

    [feat] JPA 사용을 위해 ProductRepository 생성

---
+++ src/main/java/com/notifyme/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.notifyme.repository;
+
+import com.notifyme.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
