1c4b4cab0da7bbcab66299bd3901154e0b6c57ed
rustywhite404 <rustywhite404@gmail.com>
2024-12-14 22:27:11 +0900

    [feat] Spring data redis 사용을 위한 환경설정

---
+++ src/main/java/com/notifyme/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.notifyme.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // 키를 문자열로 직렬화
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Integer.class)); // 값을 Integer로 직렬화
+        return redisTemplate;
+    }
+}
