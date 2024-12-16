8131e7d5f99f4d888258dd2a087c03103d9c0328
rustywhite404 <rustywhite404@gmail.com>
2024-12-16 11:52:39 +0900

    [feat] restockAndNotify() 수행시간 측정 테스트 코드 작성

---
+++ src/test/java/com/notifyme/service/NotificationServiceTest.java
@@ -0,0 +1,70 @@
+package com.notifyme.service;
+
+import com.notifyme.entity.Product;
+import com.notifyme.entity.ProductUserNotification;
+import com.notifyme.repository.ProductRepository;
+import com.notifyme.repository.ProductUserNotificationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class NotificationServiceTest {
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductUserNotificationRepository productUserNotificationRepository;
+    private Long productId;
+
+    @BeforeEach
+    public void setup() {
+        // 테스트용 데이터 준비 (Product와 User 알림 데이터 생성)
+        productId = prepareTestData();
+    }
+
+    @Test
+    public void testCodePerformance() {
+        // 코드 실행 시간 측정
+        long startTime = System.nanoTime();
+        notificationService.restockAndNotify(productId);
+        long endTime = System.nanoTime();
+
+        System.out.println("Code Execution Time: " + (endTime - startTime) / 1_000_000 + " ms");
+    }
+
+    private Long prepareTestData() {
+        // 1. 상품 생성
+        Product product = Product.builder()
+                .restockRound(0)
+                .stockStatus(Product.StockStatus.IN_STOCK)
+                .build();
+        product = productRepository.save(product);
+
+        //2. 500명의 알림 신청 유저 생성
+        List<ProductUserNotification> notifications = new ArrayList<>();
+        for (int i = 1; i <= 500; i++) {
+            ProductUserNotification notification = ProductUserNotification.builder()
+                    .product(product)
+                    .userId((long) i)
+                    .isActive(true)
+                    .build();
+            notifications.add(notification);
+        }
+        productUserNotificationRepository.saveAll(notifications);
+
+        return product.getId();
+
+    }
+
+
+}
